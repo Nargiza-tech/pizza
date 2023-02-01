83283c80b622f5d7e0463e6ab51ed5c4cdc76d37
Nargiza <saparbaeva.nargiza@gmail.com>
2023-02-01 17:20:45 +0600

    add files

---
+++ src/Cotigories/Cotigories.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Categories = ({categoryId, onChangeCategory}) => {
+
+
+
+
+    const categories = ["Все", "Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые",]
+
+
+    return (
+        <div className="categories">
+            <ul>
+
+                {
+                    categories.map((value, index) => {
+                        return <li key={index} onClick={() => onChangeCategory(index)}
+                                   className={categoryId === index ? 'active' : ''}>{value}</li>
+                    })
+                }
+
+            </ul>
+        </div>
+    );
+};
+
+export default Categories;
